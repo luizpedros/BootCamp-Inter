ffd786d0ab2cb45f86ad6888c1649a9b4f272466
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-25 07:59:13 -0300

    versão

---
+++ javascript/manibulando-DOM/assents/s.js
@@ -0,0 +1,17 @@
+
+var N = 6;
+
+function d() {
+
+
+
+for (let i = 1; i <= 10000; i++) {
+  if (i % N === 2){
+           
+            //return(`${i} '^2 ='${i*i}`); 
+    console.log(i); 
+  }
+        
+}
+}
+d();
